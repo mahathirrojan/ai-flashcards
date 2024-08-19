d3a3b08a6cfb3380c362d04f04ebb69b144eae5c
mahathirrojan <torbengalifreind@gmail.com>
2024-08-19 17:45:21 -0400

    bugFix

--- app/result/page.js
+++ app/result/page.js
@@ -1,6 +1,5 @@
 'use client';
 import { useEffect, useState } from "react";
-
 import { useSearchParams, useRouter} from "next/navigation";
 import { Typography, Container, CircularProgress, Box } from "@mui/material";
 
